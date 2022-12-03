f5f17cc33cec418c9041615ddeede05148289f07
Benedict <newlyf38@gmail.com>
2022-12-03 20:54:14 +0000

    created a function to handle an event in functional base components

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -6,11 +6,11 @@ import Profile from './components/profile';
 import Counter from './components/counter';
 import Destruc from './components/destruction';
 import Destruc1 from './components/destructuring1';
+import Ehifc from './components/event-handling-in-functional-component'; 
 function App() {
   return (
     <div className="App">
-      <Destruc name="Emmanu"></Destruc>
-      <Destruc1 name="Prince" age="23"></Destruc1>
+      <Ehifc />
     </div>
   );
 }
---
+++ myproject/src/components/event-handling-in-functional-component.js
@@ -0,0 +1,8 @@
+function Ehifc() {
+    let eventss = () =>{ console.log("I am tring to click a button")}
+    return (<div>
+        <button onClick={eventss}>Click here</button>
+    </div>);
+}
+
+export default Ehifc
